878cb88b38e4674d4df02bce79a6d0d0356b31d8
Kanishaka Pranjal <97291226+ReyKan-KP@users.noreply.github.com>
2024-02-02 00:01:09 +0530

    Add files via upload

---
+++ S20210010108.py
@@ -0,0 +1,35 @@
+# Kanishaka Pranjal
+# S20210010108
+# MS Assignment 1
+
+from PIL import Image, ImageFont, ImageDraw
+import os
+
+image = Image.open("S20210010108_InputImage.png")
+createImg = ImageDraw.Draw(image)
+
+print("Image size:", image.size)
+
+rochesterFontFolder = "rochester"
+calligraphyFontFolder = "Antonellie-Calligraphy-Font"
+rochesterFont = os.path.join(rochesterFontFolder, "Rochester-Regular.ttf")
+calligraphyFont = os.path.join(calligraphyFontFolder, "Morning-Dew.otf")
+
+mainFont = ImageFont.truetype(rochesterFont, 70)
+secondaryFont = ImageFont.truetype(calligraphyFont, 35)
+tertiaryFont = ImageFont.truetype(calligraphyFont, 30)
+createFont = ImageFont.truetype(rochesterFont, 20)
+
+mainText = "       Happy \n  Teacher's Day"
+secondaryText = "Thank you for imparting your knowledge unto us."
+tertiaryText = "   We will always be grateful \nto you for your support and kindness."
+createText = "Created by - \nKanishaka Pranjal \nS20210010108"
+
+createImg.text((90, 308), mainText, font=mainFont, fill="#6f694f")
+createImg.text((60, 250), secondaryText, font=secondaryFont, fill="#ee9358")
+createImg.text((120, 510), tertiaryText, font=tertiaryFont, fill="#ee9358")
+createImg.text((400, 630), createText, font=createFont, fill="#f34f58")
+
+image.save("S20210010108_OutputImage.png")
+
+image.show()
